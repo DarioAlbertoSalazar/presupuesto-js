2c49337a8958614e7c1e2186cb79bf51690f995e
Darío Salazar Leal <dario12_58@hotmail.com>
2023-09-11 19:34:31 -0600

    adding new div

--- js/app-temp.js
+++ js/app-temp.js
@@ -1,11 +1,56 @@
+let egresos = [900, 400];
+let ingresos = [9000, 400];
+let x = 0;
+
+const totalIngresos = () => {
+  let totalIngresos = 0;
+  for (const ingreso of ingresos) {
+    totalIngresos += ingreso;
+  }
+  return totalIngresos;
+};
+
+const totalEgresos = () => {
+  let totalEgreso = 0;
+  for (const egreso of egresos) {
+    totalEgreso += egreso;
+  }
+  return totalEgreso;
+};
+
+const formatoMoneda = (valor) => {
+  return valor.toLocaleString("es-MX", {
+    style: "currency",
+    currency: "MXN",
+    minimumFractionDigits: 2,
+  });
+};
+
+const formatoPorcentaje = (valor) => {
+  return valor.toLocaleString("es-MX", {
+    style: "percent",
+    minimumFractionDigits: 2,
+  });
+};
+
+const cargarCabecero = () => {
+  const presupuesto = totalIngresos() - totalEgresos();
+  const porcentajeEgreso = totalEgresos() / totalIngresos();
+  console.log(formatoMoneda(presupuesto));
+  console.log(formatoPorcentaje(porcentajeEgreso));
+};
+
+
 const agregarDato = () => {
-  let valor = parseInt(document.getElementById('valor').value);
-  let descripcion = (document.getElementById('descripcion').value);
-  let tipo = document.getElementById('tipo').value;
+  // const valor = parseInt(document.getElementById('valor'));
+  // const descripcion = (document.getElementById("descripcion"));
+  // const tipo = document.getElementById("tipo").value;
+  // console.log(document.getElementById('valor'));
 
   if (tipo === "Ingreso") {
     let newboxent = document.createElement("div");
     newboxent.innerHTML = `
+
                     <div class="elemento_descripcion">${descripcion}</div>
                     <div class="derecha limpiarEstilos">
                         <div class="elemento_valor">+${valor}</div>
@@ -19,6 +64,8 @@ const agregarDato = () => {
     nuevocontenedor.insertAdjacentElement("beforeend", newboxent);
   }
 };
+
+agregarDato();
 
 let eliminarIngreso = () => {
   let elemento = document.getElementById("lista-ingresos");
