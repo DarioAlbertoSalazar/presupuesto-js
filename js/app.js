6bb5186602d019b4aa0cc7e62edb743c055bca4f
Darío Salazar Leal <dario12_58@hotmail.com>
2023-09-13 20:54:34 -0600

    change delete functions

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 let ingresoTotal = 0;
 let egresoTotal = 0;
-let nuevoPresupuesto = 0;
+let presupuesto = 0;
 
 document.addEventListener("DOMContentLoaded", function () {
-  const nuevoPresupuesto = prompt("Ingrese el nuevo presupuesto");
-  if (nuevoPresupuesto !== null && !isNaN(parseInt(nuevoPresupuesto))) {
-    const valorPresupuestoElement = document.getElementById("presupuestoNuevo");
-    valorPresupuestoElement.innerHTML = `$${nuevoPresupuesto}`;
-    realizarCalculos(nuevoPresupuesto, ingresoTotal, egresoTotal); // Pasar valores a realizarCalculos después de actualizar
+  // presupuesto = Number(prompt("Ingrese el nuevo presupuesto"));
+  if (presupuesto !== null && !isNaN(parseInt(presupuesto))) {
+    realizarCalculos(ingresoTotal, egresoTotal);
   } else {
     alert("Ingrese un valor de presupuesto válido.");
   }
+  document.getElementById('ingresos').innerHTML = `<p>$${ingresoTotal}</p>`
+  document.getElementById('egresos').innerHTML = `<p>$${egresoTotal}</p>`
 });
 
 const agregarDato = () => {
@@ -23,7 +23,7 @@ const agregarDato = () => {
     newboxent.innerHTML = `
                     <div class="elemento_descripcion">${descripcion}</div>
                     <div class="derecha limpiarEstilos">
-                        <div class="elemento_valor">+${valor}</div>
+                        <div class="elemento_valor" id="valor_ingreso">+${valor}</div>
                         <div class="elemento_eliminar">
                             <button class="elemento_eliminar--btn" onclick="eliminarIngreso()">
                                 <ion-icon name="close-circle-outline"></ion-icon>
@@ -32,17 +32,17 @@ const agregarDato = () => {
     newboxent.setAttribute("class", "elemento limpiarEstilos");
     let nuevocontenedor = document.getElementById("lista-ingresos");
     nuevocontenedor.insertAdjacentElement("beforeend", newboxent);
+    
     ingresoTotal = ingresoTotal + valor;
+    presupuesto += valor;
     document.getElementById("ingresos").innerHTML = `<p>+${ingresoTotal}</p>`;
-    document.getElementById("valor").value = "";
-    document.getElementById("descripcion").value = "";
   } else if (tipo === "Egreso") {
     let newboxent = document.createElement("div");
     newboxent.innerHTML = `
     <div class="elemento_descripcion">${descripcion}</div>
     <div class="derecha limpiarEstilos">
-        <div class="elemento_valor">-${valor}</div>
-        <div class="elemento_porcentaje">%</div>
+        <div class="elemento_valor" id="valor_egreso">-${valor}</div>
+        <div class="elemento_porcentaje"></div>
         <div class="elemento_eliminar">
             <button class="elemento_eliminar--btn"
                     onclick="eliminarEgreso()">
@@ -54,29 +54,50 @@ const agregarDato = () => {
     let nuevocontenedor = document.getElementById("lista-egresos");
     nuevocontenedor.insertAdjacentElement("beforeend", newboxent);
     egresoTotal = egresoTotal + valor;
+    presupuesto -= valor;
     document.getElementById("egresos").innerHTML = `<p>-${egresoTotal}</p>`;
-    document.getElementById("valor").value = "";
-    document.getElementById("descripcion").value = "";
   }
-  realizarCalculos(nuevoPresupuesto, ingresoTotal, egresoTotal); // Pasar valores a realizarCalculos después de actualizar
+  realizarCalculos(ingresoTotal, egresoTotal);
 };
 
-function realizarCalculos(nuevoPresupuesto, ingresoTotal, egresoTotal) {
-  console.log("El valor del presupuesto es:", nuevoPresupuesto);
-  const saldo = nuevoPresupuesto - egresoTotal + ingresoTotal;
-  console.log("El saldo actual es:", saldo);
-  console.log("Total de ingresos:", ingresoTotal);
-  console.log("Total de egresos:", egresoTotal);
+function realizarCalculos(ingresoTotal, egresoTotal) {
+  const saldo = presupuesto - egresoTotal + ingresoTotal;
+  // console.log("El valor del presupuesto es:", presupuesto);
+  // console.log("El saldo actual es:", saldo);
+  // console.log("Total de ingresos:", ingresoTotal);
+  // console.log("Total de egresos:", egresoTotal);
+  let valorPresupuestoElement = document.getElementById("presupuestoNuevo");
+  valorPresupuestoElement.innerHTML = `$${presupuesto}`;
 }
 
 let eliminarIngreso = () => {
   let elemento = document.getElementById("lista-ingresos");
   let ingresodelete = elemento.querySelector(".elemento");
+  let quitarIngreso = parseInt(
+    document.getElementById("valor_ingreso").innerHTML
+  );
+  console.log(quitarIngreso);
+  let presupuestoTotal = document.getElementById("presupuestoNuevo").innerHTML;
+  let presupuestoO = parseInt(presupuestoTotal.replace("$", ""));
+  presupuestoO = presupuestoO - quitarIngreso;
+  let valorPresupuestoElement = document.getElementById("presupuestoNuevo");
+  valorPresupuestoElement.innerHTML = `$${presupuestoO}`;
   elemento.removeChild(ingresodelete);
 };
 
 let eliminarEgreso = () => {
   let elemento = document.getElementById("lista-egresos");
   let egresodelete = elemento.querySelector(".elemento");
+  let quitarEgreso = document.getElementById("valor_egreso").innerHTML;
+  let quitarEgreso0 = parseInt(quitarEgreso.replace("-", ""));
+  console.log(quitarEgreso0);
+  let presupuestoTotal = document.getElementById("presupuestoNuevo").innerHTML;
+  console.log(presupuestoTotal);
+  let presupuestoO = parseInt(presupuestoTotal.replace("$", ""));
+  console.log(presupuestoO);
+  presupuestoO = presupuestoO + quitarEgreso0;
+  console.log(presupuestoO);
+  let valorPresupuestoElement = document.getElementById("presupuestoNuevo");
+  valorPresupuestoElement.innerHTML = `$${presupuestoO}`;
   elemento.removeChild(egresodelete);
 };
